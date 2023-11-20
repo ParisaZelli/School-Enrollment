da6258ff05a869516006cd8a17e04c6cbb6efddf
Parisa <116458922+ParisaZelli@users.noreply.github.com>
2023-11-20 10:15:01 +0330

    Create models.py

---
+++ student/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class stud(models.Model):
+    s_name= models.CharField(max_length=30)
+    s_class= models.CharField(max_length=30)
+    s_address= models.CharField(max_length=30)
+    s_school= models.CharField(max_length=30)
+    s_email= models.EmailField(max_length=30)
+
+
