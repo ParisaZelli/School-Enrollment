448e45a537c0916682912b330c5263373b83b1c1
Parisa <116458922+ParisaZelli@users.noreply.github.com>
2023-11-20 10:14:41 +0330

    Create forms.py

---
+++ student/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class StudForm(forms.Form):
+    s_name= forms.CharField(max_length=30,label='Student name')
+    s_class= forms.CharField(max_length=30,label='Class')
+    s_address= forms.CharField(max_length=30,label='Address')
+    s_school= forms.CharField(max_length=30,label='School name')
+    s_email= forms.EmailField(max_length=30,label='Student E-Mail')
+
+
+class SForm(forms.Form):
+    s_name= forms.CharField(max_length=30,label='Student name')
